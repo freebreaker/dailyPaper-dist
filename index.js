9ff3fab30b14af6a5e09e36988b5aff3df3a226f
freebreaker <592378845@qq.com>
2017-07-24 01:53:05 +0800

    five

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+var http = require("http");
+http.createServer(function(request, response) {
+var file_path = "./index.html";
+require("fs").readFile(file_path, "binary", function(err, file) {
+                    if (err) {
+                        response.writeHead(500, {
+                            'Content-Type': 'text/plain'
+                        });
+                        response.end(err);
+                    } 
+                    else {
+                        response.writeHead(200, {
+                            'Content-Type': 'text/html'
+                        });
+                        response.write(file, "binary");
+                        response.end();
+                    }
+                }); }).listen(8888);
+console.log("服务启动...");
